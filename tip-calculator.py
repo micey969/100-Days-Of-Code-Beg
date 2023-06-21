44183ca985ef9549dd46c008222c8a9c124374ac
micey969 <micey_969@hotmail.com>
2023-06-21 17:57:23 -0400

    correct tip calculation

---
+++ tip-calculator.py
@@ -0,0 +1,14 @@
+'''
+100 Days of Code
+
+Day 2 - Tip Calculator
+'''
+
+print("Welcome to the tip calculator.")
+bill =  float(input("What was the total bill? "))
+people = int(input("How many people to split the bill? "))
+tip = int(input("What percentage tip would you lik to give? 10, 12, or 15? "))
+
+payment = (bill/people)*1.12
+
+print("Each person should pay: $%.2f" %payment)
