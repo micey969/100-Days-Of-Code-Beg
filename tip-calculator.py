c638e80f42d9232936b45b66bccc8946e241668f
micey969 <micey_969@hotmail.com>
2023-06-24 21:07:32 -0400

    Add day 4 challenge

--- tip-calculator.py
+++ tip-calculator.py
@@ -9,6 +9,6 @@ bill =  float(input("What was the total bill? "))
 people = int(input("How many people to split the bill? "))
 tip = int(input("What percentage tip would you lik to give? 10, 12, or 15? "))
 
-payment = (bill/people)*1.12
+payment = (bill/people) + ((bill/people)*tip/100)
 
 print("Each person should pay: $%.2f" %payment)
