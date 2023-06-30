0bde0e53500beb53f854ecc5395e13a53c77ba3e
Shazelle <micey_969@hotmail.com>
2023-06-30 22:06:29 +0100

    Rename files

---
+++ band_name_generator.py
@@ -0,0 +1,13 @@
+'''
+100 Days of Code
+
+Day 1 - Band Name Generator
+'''
+print("Welcome to the Band Name Generator.")
+print("What's the name of the city you grew up in?")
+city = input()
+
+print("What's your pet's name?")
+pet = input()
+
+print("Your band name could be " + city + " "+ pet)
